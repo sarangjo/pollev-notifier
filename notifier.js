a83bdeafc37ea004c16baf0024d59efce0a9ca8a
Sarang Joshi <sarangj@msn.com>
2018-05-30 02:28:31 -0700

    Basic outline

--- notifier.js
+++ notifier.js
@@ -1,19 +1,39 @@
 let notif = "NOTIFICATION";
 
-browser.alarms.create("", {periodInMinutes: 0.1});
+// https://stackoverflow.com/a/44670502/3321184
+function respondToVisibility(element, callback) {
+  var options = {
+    root: document.documentElement
+  };
 
-browser.alarms.onAlarm.addListener(function(alarm) {
+  var observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      callback(entry.intersectionRatio > 0);
+    });
+  }, options);
+
+  observer.observe(element);
+}
+
+respondToVisibility(document.getElementsByClassName('lobby')[0], isVisible => {
+  console.log(isVisible);
+  if (isVisible) {
+    notify();
+  }
+});
+
+function notify() {
   browser.notifications.create(notif, {
     // TODO add sound?
     "type": "basic",
     "title": "Notif",
     "message": "Message"
   });
-});
 
-browser.browserAction.onClicked.addListener(() => {
-  let clearing = browser.notifications.clear(notif);
-  clearing.then(() => {
-    console.log("cleared notif");
+  browser.browserAction.onClicked.addListener(() => {
+    let clearing = browser.notifications.clear(notif);
+    clearing.then(() => {
+      console.log("cleared notif");
+    });
   });
-});
+}
